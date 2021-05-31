d6a282f9275eca085931ccbc8f65bd5b7c8653cf
Keller0Junge <Keller-Junge@web.de>
2021-05-31 12:05:06 +0200

    Übersicht und email

--- InstallScript.sh
+++ InstallScript.sh
@@ -6,13 +6,14 @@ wp core download
 wp config create --dbname=d0369158 --dbuser=d0369158 --dbpass=secure
 echo wp db create
 
-wp core install --url=test108511.test-account.com --title=TestSeite --admin_user=admin --admin_password=adminpsw --admin_email=becker-janik@web.de
+wp core install --url=test108511.test-account.com --title=TestSeite --admin_user=admin --admin_password=adminpsw --admin_email=test@web.de
 
 echo Die Basis Installation von WordPress ist abgeschlossen
 echo Ab Hier werden Themes und Plugins gelöscht und Installiert
 
 wp theme install blocksy
 wp theme install kadence --activate
+
 wp theme delete twentytwentyone
 wp theme delete twentytwenty
 wp theme delete twentynineteen
